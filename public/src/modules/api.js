79656338096eb4503fbeff3a2d2422e0281c6d4f
HandreyGama <handreygama.profissional@gmail.com>
2026-04-06 17:25:30 -0300

    adicionando as capas dos livros

--- public/src/modules/api.js
+++ public/src/modules/api.js
@@ -20,7 +20,7 @@ export async function pegar_livros_por_titulo(titulo){
 
 }
 export async function pegar_livros_por_autor(autor){
-    const query = OPEN_LIBRARY_URL + "search.json" + "?author=" + encodeURIComponent(titulo)
+    const query = OPEN_LIBRARY_URL + "search.json" + "?author=" + encodeURIComponent(autor)
     console.log(query)
     try{
         const response = await fetch(query)
@@ -58,25 +58,22 @@ export async function pegar_livros_por_tema(tema){
 }
 
 
-export async function pegar_capa_livro(ISBN_code){
-    const url = `https://covers.openlibrary.org/b/isbn/${ISBN_code}-M.jpg`
-    
-    try{
-        const response = await fetch(url)
-
-        if(!response.ok){
-            throw new Error("ERRO NA REDE: " + response.status)
-        }
-
-        const blob = await response.blob()
-        return URL.createObjectURL(blob)
-
-    } catch(error){
-        console.log(error.message)
-    }
-
+export async function pegar_codigo_isbn(code){
+    if (!code) return null;
+    const url = `https://openlibrary.org/books/${code}.json`
+    const res = await fetch(url)
+    const edicao = await res.json();
+    return edicao.isbn_13?.[0] || edicao.isbn_10?.[0] || null;
 }
 
-pegar_livros_por_tema("love").then(data => {
+export  function pegar_capa_livro(cover_edition_key){
+    if (!cover_edition_key) return null;
+    const url = `https://covers.openlibrary.org/b/olid/${cover_edition_key}-M.jpg`
+    
+    return url
+}
+
+
+pegar_livros_por_autor("rowling").then(data =>{
     console.log(data)
 })
---
+++ public/templates/home/livros_carrossels.js
@@ -0,0 +1,23 @@
+import { pegar_livros_por_autor, pegar_capa_livro } from "../../src/modules/api.js"
+
+document.addEventListener('DOMContentLoaded',async () =>{
+    const div_carrossels = document.getElementById('livros-carossels')
+    const div_livros_autores = document.getElementById('livros-autores')
+    const livros_tolkien = await pegar_livros_por_autor('rowling')
+    const titulo_autor = document.getElementById("livros-autor")
+    titulo_autor.innerText = "rowling"
+    const lista_livros = livros_tolkien.docs
+    
+    for (const element of lista_livros) {
+        if (!element.cover_edition_key) continue; // 🔥 ESSENCIAL
+        const card_livros = document.createElement("div")
+        const cover_edition_key = element.cover_edition_key
+        const link_capa_livro = pegar_capa_livro(cover_edition_key)
+        console.log(link_capa_livro)
+        card_livros.innerHTML = `
+        <h3 class="livros-cards"> ${element.title} </h3>
+        <img src="${link_capa_livro}" alt="">
+        `
+        div_carrossels.appendChild(card_livros)
+    };
+})
