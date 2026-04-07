53541aaeccfdb0b1f21034d6a5ca10cd13f27af5
HandreyGama <handreygama.profissional@gmail.com>
2026-04-07 15:12:07 -0300

    fix:(home.html) concertando os bugs de livros escapando do container feat(livros_carrossel): adicionando botoes no carrossel

--- public/templates/home/livros_carrossels.js
+++ public/templates/home/livros_carrossels.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded',async () =>{
     const div_livros_autores = document.getElementById('livros-autores')
     const livros_tolkien = await pegar_livros_por_autor('colleen-hoover')
     const titulo_autor = document.getElementById("livros-autor")
+    const btnPrev = document.getElementById("livros-carrossel-button-left")
+    const btnNext = document.getElementById("livros-carrossel-button-right")
+    let currentBook = 0;
     titulo_autor.innerText = "colleen hoover"
     const lista_livros = livros_tolkien.docs
     
@@ -16,8 +19,42 @@ document.addEventListener('DOMContentLoaded',async () =>{
         console.log(link_capa_livro)
         card_livros.innerHTML = `
         <h3 class="livros-cards"> ${element.title} </h3>
-        <img src="${link_capa_livro}" alt="">
+        <img class="livro-capa" src="${link_capa_livro}" alt="">
         `
         div_carrossels.appendChild(card_livros)
     };
+    const livros = document.querySelectorAll('.livro-capa')
+    let livro_em_foco = livros[currentBook]
+    function centralizarItem(item) {
+        const itemCenter = item.offsetLeft + item.offsetWidth / 2
+        const containerCenter = div_carrossels.offsetWidth / 2
+
+        let scrollPara = itemCenter - containerCenter
+        div_carrossels.scrollTo({
+            left: scrollPara,
+            behavior: 'smooth'
+        })
+    }
+    btnNext.addEventListener('click',() =>{
+        if(livro_em_foco){
+            livro_em_foco.classList.remove('on')
+        }
+ 
+        currentBook += 1;
+        if (currentBook >= livros.length) currentBook = livros.length - 1
+        livro_em_foco = livros[currentBook]
+        livro_em_foco.classList.add('on')
+        centralizarItem(livro_em_foco)
+    })
+
+        btnPrev.addEventListener('click',() =>{
+        if(livro_em_foco){
+            livro_em_foco.classList.remove('on')
+        }
+        currentBook -= 1;
+        if (currentBook < 0) currentBook = 0
+        livro_em_foco = livros[currentBook] 
+        livro_em_foco.classList.add('on')
+        centralizarItem(livro_em_foco)
+    })
 })
