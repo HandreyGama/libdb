b58890680f3a0ba59585ccb0f6f6d04f7ff7e41e
Olivia <viviixrz@gmail.com>
2026-04-07 01:15:46 -0300

    style(login, home): pequenos ajustes no css

--- public/templates/home/livros_carrossels.js
+++ public/templates/home/livros_carrossels.js
@@ -3,13 +3,13 @@ import { pegar_livros_por_autor, pegar_capa_livro } from "../../src/modules/api.
 document.addEventListener('DOMContentLoaded',async () =>{
     const div_carrossels = document.getElementById('livros-carossels')
     const div_livros_autores = document.getElementById('livros-autores')
-    const livros_tolkien = await pegar_livros_por_autor('hoover')
+    const livros_tolkien = await pegar_livros_por_autor('colleen-hoover')
     const titulo_autor = document.getElementById("livros-autor")
     titulo_autor.innerText = "colleen hoover"
     const lista_livros = livros_tolkien.docs
     
     for (const element of lista_livros) {
-        if (!element.cover_edition_key) continue; // 🔥 ESSENCIAL
+        if (!element.cover_edition_key) continue;
         const card_livros = document.createElement("div")
         const cover_edition_key = element.cover_edition_key
         const link_capa_livro = pegar_capa_livro(cover_edition_key)
