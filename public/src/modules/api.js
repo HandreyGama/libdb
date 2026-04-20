d775356039aedfd808d897c842c3819e1f134e5c
vitao-al <vm74531@gmail.com>
2026-04-20 13:53:06 -0300

    refactor(api.js): melhorias

--- public/src/modules/api.js
+++ public/src/modules/api.js
@@ -1,60 +1,42 @@
 const OPEN_LIBRARY_URL = 'https://openlibrary.org/'
+const FETCH_TIMEOUT_MS = 7000
 
+async function fetchJsonWithTimeout(url, fallbackData) {
+    const controller = new AbortController()
+    const timeoutId = setTimeout(() => controller.abort(), FETCH_TIMEOUT_MS)
+
+    try {
+        const response = await fetch(url, {
+            signal: controller.signal,
+            headers: { Accept: 'application/json' }
+        })
+
+        if (!response.ok) {
+            throw new Error('ERRO NA REDE: ' + response.status)
+        }
+
+        return await response.json()
+    } catch (error) {
+        console.log('Falha ao buscar livros:', error.message)
+        return fallbackData
+    } finally {
+        clearTimeout(timeoutId)
+    }
+}
 
 export async function pegar_livros_por_titulo(titulo){
-    const query = OPEN_LIBRARY_URL + "search.json" + "?title=" + encodeURIComponent(titulo)
-    console.log(query)
-    try{
-        const response = await fetch(query)
-
-        if(!response.ok){
-            throw new Error("ERRO NA REDE: " + response.status)
-        }
-
-        const data = await response.json()
-        return data
-
-    } catch(error){
-        console.log(error.message)
-    }
-
+    const query = OPEN_LIBRARY_URL + 'search.json?title=' + encodeURIComponent(titulo)
+    return await fetchJsonWithTimeout(query, { docs: [] })
 }
+
 export async function pegar_livros_por_autor(autor){
-    const query = OPEN_LIBRARY_URL + "search.json" + "?author=" + encodeURIComponent(autor)
-    console.log(query)
-    try{
-        const response = await fetch(query)
-
-        if(!response.ok){
-            throw new Error("ERRO NA REDE: " + response.status)
-        }
-
-        const data = await response.json()
-        return data
-
-    } catch(error){
-        console.log(error.message)
-    }
-
+    const query = OPEN_LIBRARY_URL + 'search.json?author=' + encodeURIComponent(autor)
+    return await fetchJsonWithTimeout(query, { docs: [] })
 }
 
 export async function pegar_livros_por_tema(tema){
-    const query = OPEN_LIBRARY_URL + "subjects" + "/" + tema + " .json"
-    console.log(query)
-    try{
-        const response = await fetch(query)
-
-        if(!response.ok){
-            throw new Error("ERRO NA REDE: " + response.status)
-        }
-
-        const data = await response.json()
-        return data
-
-    } catch(error){
-        console.log(error.message)
-    }
-
+    const query = OPEN_LIBRARY_URL + 'subjects/' + encodeURIComponent(tema) + '.json'
+    return await fetchJsonWithTimeout(query, { works: [] })
 }
 
 
@@ -72,8 +54,3 @@ export  function pegar_capa_livro(cover_edition_key){
     
     return url
 }
-
-
-pegar_livros_por_autor("rowling").then(data =>{
-    console.log(data)
-})
